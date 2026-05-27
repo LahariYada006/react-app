5fd7216b3c4047f95e9201f559a224e8ec0b4ef1
LahariYada006 <lahariyada06@gmail.com>
2026-05-27 12:38:52 +0530

    fixed index,js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,33 +3,41 @@ import dotenv from "dotenv"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import mongoose from "mongoose"
-
+import authRoutes from "./routes/AuthRoutes.js"
+import contactsRoutes from "./routes/ContactRoutes.js"
+import setupSocket from "./socket.js"
+import messagesRoutes from "./routes/MessageRoutes.js"
+import channelRoutes from "./routes/ChannelRoutes.js"
 
 dotenv.config();
 
-const app= express();
+const app = express();
 const port = process.env.PORT || 3001;
-const databaseURL =  process.env.DATA_BASE_URL;
-
+const databaseURL = process.env.DATA_BASE_URL;
 
 app.use(cors({
-    origin:process.env.ORIGIN,
-    method:["GET","POST","PUT","PATCH","DELETE"],
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
-})
-);
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.options("*", cors());
-
-
-app.use("/upload/profiles",express.static("uploads/profiles"));
-
-
 
 app.use(cookieParser());
 app.use(express.json());
+app.use("/uploads/profiles", express.static("uploads/profiles"));
+app.use("/uploads/files", express.static("uploads/files"));
 
-const server = app.listen(port,()=>{
+app.use('/api/auth', authRoutes);
+app.use("/api/contacts", contactsRoutes);
+app.use('/api/messages', messagesRoutes);
+app.use("/api/channel", channelRoutes);
+
+const server = app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${port}`)
 });
 
-mongoose.connect(databaseURL).then(()=>console.log('DB Connection Successful')).catch(err=>console.log(err.message));
+setupSocket(server);
+
+mongoose.connect(databaseURL).then(() => console.log('DB Connection Successful')).catch(err => console.log(err.message));
