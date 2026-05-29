f7f5788f873f5356102cecbcfa5109cb7e44e87c
LahariYada006 <lahariyada06@gmail.com>
2026-05-29 07:47:17 +0530

    temp test

--- server/index.js
+++ server/index.js
@@ -1,44 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import mongoose from "mongoose"
-import authRoutes from "./routes/AuthRoutes.js"
-import contactsRoutes from "./routes/ContactRoutes.js"
-import setupSocket from "./socket.js"
-import messagesRoutes from "./routes/MessageRoutes.js"
-import channelRoutes from "./routes/ChannelRoutes.js"
-
-dotenv.config();
+import express from "express";
 
 const app = express();
 
-const port = process.env.PORT || 3001;
-const databaseURL = process.env.DATA_BASE_URL;
-
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://react-app-u2pt.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true,
-}));
-
-app.use(cookieParser());
-app.use(express.json());
-app.use("/uploads/profiles", express.static("uploads/profiles"));
-app.use("/uploads/files", express.static("uploads/files"));
-
-app.use('/api/auth', authRoutes);
-app.use("/api/contacts", contactsRoutes);
-app.use('/api/messages', messagesRoutes);
-app.use("/api/channel", channelRoutes);
-
-const server = app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running at http://localhost:${port}`)
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
-setupSocket(server);
+app.get("/health", (req, res) => {
+  res.json({ success: true });
+});
 
-mongoose.connect(databaseURL).then(() => console.log('DB Connection Successful')).catch(err => console.log(err.message));
+const port = process.env.PORT || 3001;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log("Running on", port);
+});
+
